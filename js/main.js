7d6b68a6518ae021c71a42f2c06e62970e4c2b38
Nina <mysha76@mail.ru>
2021-03-23 18:15:27 +0300

    Реализовано открытие модального окна корзины. Реализована плавная прокрутка в начало страницы

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// cart
+const buttonCart = document.querySelector('.button-cart'),
+	modalCart = document.querySelector('#modal-cart'),
+	modalClose = document.querySelector('.modal-close');
+
+
+const openModal = function () {
+	modalCart.classList.add('show');
+};
+
+const closeModal = function () {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+// scroll smooth
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', function (event) {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		});
+	}
+};
