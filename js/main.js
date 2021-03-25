0fe86a9f18377684ab1ce6d63525af1613bfcd25
Nina <mysha76@mail.ru>
2021-03-25 12:25:52 +0300

    Получение товаров. Создание и вывод карточек товаров.

--- js/main.js
+++ js/main.js
@@ -33,10 +33,10 @@ modalCart.addEventListener('click', event => {
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-	for (let i = 0; i < scrollLinks.length; i++) {
-		scrollLinks[i].addEventListener('click', event => {
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', event => {
 			event.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start',
@@ -44,3 +44,71 @@ modalCart.addEventListener('click', event => {
 		});
 	}
 };
+
+// goods
+
+const more = document.querySelector('.more'),
+	navigationLink = document.querySelectorAll('.navigation-link'),
+	longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function () {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Ошибочка вышла: ' + result.status;
+	}
+	return await result.json();
+};
+
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${objCard.label ?
+			`<span class="label">${objCard.label}</span>` : ''}
+			<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+			<h3 class="goods-title">${objCard.name}</h3>
+			<p class="goods-description">${objCard.description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+				<span class="button-price">$${objCard.price}</span>
+			</button>
+		</div>
+	`;
+
+	return card;
+};
+
+const renderCards = function (data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);
+	cards.forEach(function (card) {
+		longGoodsList.append(...cards)
+	});
+	document.body.classList.add('show-goods');
+};
+
+more.addEventListener('click', function (event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
+
+const filterCards = function (field, value) {
+	getGoods()
+		.then(function (data) {
+			const filteredGoods = data.filter(function (good) {
+				return good[field] === value;
+			});
+			return filteredGoods;
+		})
+		.then(renderCards);
+};
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function (event) {
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field, value);
+	});
+});
